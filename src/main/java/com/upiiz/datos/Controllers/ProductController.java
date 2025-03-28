d9558a5ac679cb90f571a1e1f317be625baaa680
Erick <albarazork@gmail.com>
2025-03-28 11:46:07 -0600

    third commit

--- src/main/java/com/upiiz/datos/Controllers/ProductController.java
+++ src/main/java/com/upiiz/datos/Controllers/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class ProductController {
 
-    @GetMapping("/products")
+    @GetMapping()
     public String index() {
         return "listado-productos";
     }
