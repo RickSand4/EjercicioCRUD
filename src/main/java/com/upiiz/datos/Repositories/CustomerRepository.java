0c1b0febccb147b9d74410e201aa68ede3c57d12
Erick <albarazork@gmail.com>
2025-03-31 10:22:13 -0600

    product repository

---
+++ src/main/java/com/upiiz/datos/Repositories/CustomerRepository.java
@@ -0,0 +1,5 @@
+package com.upiiz.datos.Repositories;
+
+public interface CustomerRepository {
+    //CRUD
+}
--- src/main/java/com/upiiz/datos/Repositories/ProductRepository.java
+++ src/main/java/com/upiiz/datos/Repositories/ProductRepository.java
@@ -1,11 +1,21 @@
 package com.upiiz.datos.Repositories;
 
+import com.upiiz.datos.Models.ProductModel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
 public interface ProductRepository {
     //Menu
-    //todos los productos
-    //Un solo producto por su id
-    //agregar un producto
-    //actualizar un producto
-    //eliminar un producto
 
+    //todos los productos
+    List<ProductModel> findAll();
+    //Un solo producto por su id
+    ProductModel findById(int id);
+    //agregar un producto
+    void save(ProductModel product);
+    //actualizar un producto
+    void update(ProductModel product);
+    //eliminar un producto por su id
+    void delete(ProductModel product);
 }
